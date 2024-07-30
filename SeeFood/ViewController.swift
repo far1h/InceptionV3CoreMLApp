f3ae40ba7864176bbb4a127a61052d74b9f9c65a
far1h <farihmuhammad45@gmail.com>
2024-07-30 17:44:02 +0700

    displaying results on navigation bar

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -44,7 +44,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results = request.results as? [VNClassificationObservation] else {
                 fatalError("Model failed to process image")
             }
-            print(results)
+            if let firstResult = results.first {
+//                if firstResult.identifier.contains("hotdog") {
+//                    self.navigationItem.title = "Hotdog!"
+//                } else {
+//                    self.navigationItem.title = "Not Hotdog!"
+//                }
+                self.navigationItem.title = firstResult.identifier
+            }
         }
         let handler = VNImageRequestHandler(ciImage: image)
         do {
